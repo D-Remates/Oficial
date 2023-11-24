db3f548232d889a8571a24d3cfb49128dcfa6d8c
D-Remates <151585276+D-Remates@users.noreply.github.com>
2023-11-24 18:11:16 -0500

    Update script2.js

--- js/script2.js
+++ js/script2.js
@@ -18,7 +18,7 @@ popupWhatsApp = () => {
   let msg = document.getElementById('whats-in').value;
   let relmsg = msg.replace(/ /g,"%20");
      
-   window.open('https://wa.me/573132684287?text='+relmsg, '_blank'); 
+   window.open('https://wa.me/573132690653?text='+relmsg, '_blank'); 
   
   });
 
